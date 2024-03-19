1004632044bdb227b6333d5f47309711ba8032d0
OliverEC <oliverec04@gmail.com>
2024-03-19 10:59:02 +0100

    test comp

--- App/App.tsx
+++ App/App.tsx
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Test from './components/test';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your gay app!</Text>
+      <Test></Test>
       <StatusBar style="auto" />
     </View>
   );
---
+++ App/components/test.tsx
@@ -0,0 +1,5 @@
+import { Text } from "react-native"
+
+export default function Test() {
+    return <Text>Greg er straight</Text>
+}
