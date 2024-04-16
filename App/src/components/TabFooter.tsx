96dc5386d992925ed222c13579ad6663eafde402
Hugh Mungus <mtlnielsen@gmail.com>
2024-04-16 15:28:08 +0200

    added user login page, and added it to the footer

--- App/src/components/TabFooter.tsx
+++ App/src/components/TabFooter.tsx
@@ -1,7 +1,7 @@
 import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
 import Home from '../screens/Home';
 import AddFood from '../screens/AddFood/AddFood';
-
+import LoginPage from '../screens/LoginPage';
 
 export default function TabFooter()
 {
@@ -11,6 +11,7 @@ export default function TabFooter()
         <Tab.Navigator>
             <Tab.Screen name="Home" component={Home} />
             <Tab.Screen name="AddFood" component={AddFood} />
+            <Tab.Screen name="LoginPage" component={LoginPage} />
         </Tab.Navigator>
     );
 }
---
+++ App/src/screens/LoginPage.tsx
@@ -0,0 +1,16 @@
+import { View, Text, TextInput } from "react-native";
+import { textStyles } from "../styles/textStyles";
+
+export default function LoginPage()
+{
+    return (
+        <View>
+            {/* <Text className="italic text-orange-600">Login screen</Text> */}
+            <Text style={textStyles.pageTitle}>Login screen</Text>
+            <Text className="text-3xl">Username</Text>
+            <TextInput placeholder="Enter your username" />
+            <Text className="text-3xl">Password</Text>
+            <TextInput placeholder="Enter your password" secureTextEntry={true} />
+        </View>
+    );
+}
