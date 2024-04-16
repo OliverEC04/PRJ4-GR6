aa72b8b9cc807912422c1afe1a83d8699f9f004e
OliverEC <oliverec04@gmail.com>
2024-04-16 15:30:06 +0200

    push

---
+++ App/src/components/StatBar.tsx
@@ -0,0 +1,15 @@
+import { View, Text } from "react-native-reanimated/lib/typescript/Animated";
+
+export default function StatBar({})
+{
+    return (
+        <>
+            <Text>
+                Title ting
+            </Text>
+            <View>
+                <Text></Text>
+            </View>
+        </>
+    );
+}
--- App/src/screens/Home.tsx
+++ App/src/screens/Home.tsx
@@ -1,11 +1,12 @@
 import { View, Text } from "react-native";
-
+import StatBar from "../components/StatBar";
 
 export default function Home()
 {
     return (
         <View>
-            <Text className="italic text-orange-600">Home screen</Text>
+            <Text className="italic text-orange-600">GYMRATS</Text>
+            <StatBar></StatBar>
         </View>
     );
 }
