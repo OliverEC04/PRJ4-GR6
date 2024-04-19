623657fa6ec4ea284f7455b5dce89aab618cfe8e
au725202 <49028947+Oppai-Loli@users.noreply.github.com>
2024-04-19 11:08:45 +0200

    commit

--- App/src/components/TabFooter.tsx
+++ App/src/components/TabFooter.tsx
@@ -3,14 +3,15 @@ import Home from "../screens/Home";
 import AddFood from "../screens/AddFood/AddFood";
 import infoPage from "../screens/infoPage/infoPage";
 
-export default function TabFooter() {
-  const Tab = createMaterialBottomTabNavigator();
+export default function TabFooter()
+{
+    const Tab = createMaterialBottomTabNavigator();
 
-  return (
-    <Tab.Navigator>
-      <Tab.Screen name="Home" component={Home} />
-      <Tab.Screen name="AddFood" component={AddFood} />
-      <Tab.Screen name="infoPage" component={infoPage} />
-    </Tab.Navigator>
-  );
+    return (
+        <Tab.Navigator>
+            <Tab.Screen name="Home" component={Home} />
+            <Tab.Screen name="AddFood" component={AddFood} />
+            <Tab.Screen name="infoPage" component={infoPage} />
+        </Tab.Navigator>
+    );
 }
