67aa75edc1e1b6472069194e711b97ba1f4f417b
Hugh Mungus <mtlnielsen@gmail.com>
2024-04-22 15:57:12 +0200

    fixed user creation function

--- App/src/models/Server.ts
+++ App/src/models/Server.ts
@@ -1,7 +1,7 @@
 class Server {
     private url: string;
 
-    constructor(url: string = "https://brief-oriole-causal.ngrok-free.app/rest_api/api") {
+    constructor(url: string = "https://brief-oriole-causal.ngrok-free.app/rest_api/") {
         this.url = url;
     }
     
@@ -14,14 +14,21 @@ class Server {
     public async registerUser(nameArg: string, passwordArg: string, emailArg: string){
         try {
             console.log("fetching... ");
-            await fetch(this.url+"/Account/Register", {
+            const response = await fetch(this.url + "Account/Register", {
                 method: 'POST',
+                headers: {
+                    'Accept': '*/*',
+                    'Content-Type': 'application/json'
+                },
                 body: JSON.stringify({
                     fullName: nameArg,
                     password: passwordArg,
                     email: emailArg
                 })
             });
+            if (!response.ok) {
+                throw new Error(`HTTP error! Status: ${response.status}`);
+            }
         }
         catch (error) {
             console.error('Error registering user:', error);
