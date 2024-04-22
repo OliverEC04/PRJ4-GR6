491d2bf748891e576dd169a7cea5e3407d060f4a
lukas tast <lukasrtast@yahoo.dk>
2024-04-22 16:38:38 +0200

    Added onChangeText

--- App/src/styles/HomeStyle.ts
+++ App/src/styles/HomeStyle.ts
@@ -1,17 +1,17 @@
 import { StyleSheet } from "react-native";
 
 export default StyleSheet.create({
-    container: {
-        display: "flex",
-        alignItems: "center",
-    },
-    logoCont: {
-        width: "10%",
-        height: "10%",
-        flex: 1,
-    },
-    logo: {
-        width: "60%",
-        height: 200,
-    },
+  container: {
+    display: "flex",
+    alignItems: "center",
+  },
+  logoCont: {
+    width: "10%",
+    height: "10%",
+    flex: 1,
+  },
+  logo: {
+    width: "60%",
+    height: 200,
+  },
 });
