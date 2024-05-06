bd848a5731d1809d25449183d02633889534cf9a
Hugh Mungus <mtlnielsen@gmail.com>
2024-05-06 15:06:28 +0200

    fixed duplicate functions

--- App/src/models/Server.ts
+++ App/src/models/Server.ts
@@ -2,69 +2,69 @@ import { User, currentUser } from "./User";
 // import AsyncStorage from '@react-native-async-storage/async-storage';
 
 class Server {
-  private url: string;
+    private url: string;
 
-  constructor(
-    url: string = "https://brief-oriole-causal.ngrok-free.app/rest_api/"
-  ) {
-    this.url = url;
-  }
-  // api/me
-
-  public async registerUser(
-    nameArg: string,
-    passwordArg: string,
-    emailArg: string
-  ) {
-    try {
-      console.log("fetching... ");
-      const response = await fetch(this.url + "Account/Register", {
-        method: "POST",
-        headers: {
-          Accept: "*/*",
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          fullName: nameArg,
-          password: passwordArg,
-          email: emailArg,
-        }),
-      });
-      if (!response.ok) {
-        throw new Error(`HTTP error! Status: ${response.status}`);
-      }
-    } catch (error) {
-      console.error("Error registering user:", error);
+    constructor(
+        url: string = "https://brief-oriole-causal.ngrok-free.app/rest_api/"
+    ) {
+        this.url = url;
     }
-  }
+    // api/me
 
-  // en put metode til at opdatere user data
-
-  public async getUserInfo(): Promise<User> {
-    const response = await fetch(
-      "https://brief-oriole-causal.ngrok-free.app/rest_api/Account/me",
-      {
-        // TJEK URL
-        method: "GET",
-        headers: {
-          Authorization: "Bearer " + currentUser.token,
-        },
-      }
-    );
-
-    if (!response.ok) {
-      throw new Error(`HTTP error! Status: ${response.status}`);
+    public async registerUser(
+        nameArg: string,
+        passwordArg: string,
+        emailArg: string
+    ) {
+        try {
+            console.log("fetching... ");
+            const response = await fetch(this.url + "Account/Register", {
+                method: "POST",
+                headers: {
+                    Accept: "*/*",
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                    fullName: nameArg,
+                    password: passwordArg,
+                    email: emailArg,
+                }),
+            });
+            if (!response.ok) {
+                throw new Error(`HTTP error! Status: ${response.status}`);
+            }
+        } catch (error) {
+            console.error("Error registering user:", error);
+        }
     }
-    const data = await response.json();
-    var userInfo = new User("dummy mail", "dummy name");
-    userInfo.calories = data.calories;
-    userInfo.proteins = data.proteins;
-    userInfo.carbs = data.carbs;
-    userInfo.fats = data.fats;
-    userInfo.water = data.water;
-    userInfo.targetWeight = data.targetWeight;
-    return userInfo;
-  }
+
+    // en put metode til at opdatere user data
+
+    public async getUserInfo(): Promise<User> {
+        const response = await fetch(
+            "https://brief-oriole-causal.ngrok-free.app/rest_api/Account/me",
+            {
+                // TJEK URL
+                method: "GET",
+                headers: {
+                    Authorization: "Bearer " + currentUser.token,
+                },
+            }
+        );
+
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        const data = await response.json();
+        var userInfo = new User("dummy mail", "dummy name");
+        userInfo.calories = data.calories;
+        userInfo.proteins = data.proteins;
+        userInfo.carbs = data.carbs;
+        userInfo.fats = data.fats;
+        userInfo.water = data.water;
+        userInfo.targetWeight = data.targetWeight;
+        return userInfo;
+    }
 
     public async loginUser(nameArg: string, passwordArg: string) {
         try {
@@ -89,7 +89,7 @@ class Server {
             }
 
             localStorage.setItem('jwtToken', currentUser.token);
-            
+
         }
         catch (error) {
             console.error('Error logging in:', error);
@@ -99,25 +99,13 @@ class Server {
     public async getUser(): Promise<void> {
         await fetch(this.url + `GetUser`)
             .then((response) => {
-                if (response.ok)
-                    return response.json();
-                else
-                    throw new Error("getUser fucked up :(");
+                if (response.ok) return response.json();
+                else throw new Error("getUser fucked up :(");
             })
             .then((data) => {
                 console.debug(data);
             });
     }
-  public async getUser(): Promise<void> {
-    await fetch(this.url + `GetUser`)
-      .then((response) => {
-        if (response.ok) return response.json();
-        else throw new Error("getUser fucked up :(");
-      })
-      .then((data) => {
-        console.debug(data);
-      });
-  }
 }
 
 export default new Server();
