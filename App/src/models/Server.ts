33e1abf9f0e4385dc763c33751497e59f0a102e4
OliverEC <oliverec04@gmail.com>
2024-05-10 12:41:23 +0200

    good place

--- App/src/models/Server.ts
+++ App/src/models/Server.ts
@@ -2,109 +2,98 @@ import { User, currentUser } from "./User";
 // import AsyncStorage from '@react-native-async-storage/async-storage';
 
 class Server {
-  private url: string;
+	private url: string;
 
-  constructor(url: string = "https://brief-oriole-causal.ngrok-free.app/") {
-    this.url = url;
-  }
-  // api/me
+	constructor(url: string = "https://brief-oriole-causal.ngrok-free.app/") {
+		this.url = url;
+	}
+	// api/me
 
-  public async registerUser(
-    nameArg: string,
-    passwordArg: string,
-    emailArg: string
-  ) {
-    try {
-      console.log("fetching... ");
-      const response = await fetch(this.url + "Account/Register", {
-        method: "POST",
-        headers: {
-          Accept: "*/*",
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          fullName: nameArg,
-          password: passwordArg,
-          email: emailArg,
-        }),
-      });
-      if (!response.ok) {
-        throw new Error(`HTTP error! Status: ${response.status}`);
-      }
-    } catch (error) {
-      console.error("Error registering user:", error);
-    }
-  }
+	public async registerUser(
+		nameArg: string,
+		passwordArg: string,
+		emailArg: string
+	) {
+		try {
+			console.log("fetching... ");
+			const response = await fetch(this.url + "Account/Register", {
+				method: "POST",
+				headers: {
+					Accept: "*/*",
+					"Content-Type": "application/json",
+				},
+				body: JSON.stringify({
+					fullName: nameArg,
+					password: passwordArg,
+					email: emailArg,
+				}),
+			});
+			if (!response.ok) {
+				throw new Error(`HTTP error! Status: ${response.status}`);
+			}
+		} catch (error) {
+			console.error("Error registering user:", error);
+		}
+	}
 
-  // en put metode til at opdatere user data
+	// en put metode til at opdatere user data
 
-  public async getUserInfo(): Promise<User> {
-    const response = await fetch(`${this.url}AppUser/me`, {
-      // TJEK URL
-      method: "GET",
-      headers: {
-        Authorization: "Bearer " + currentUser.token,
-      },
-    });
+	public async getUserInfo(): Promise<User> {
+		const response = await fetch(`${this.url}AppUser/me`, {
+			// TJEK URL
+			method: "GET",
+			headers: {
+				Authorization: "Bearer " + currentUser.token,
+			},
+		});
 
-    console.log(currentUser.token);
+		if (!response.ok) {
+			throw new Error(`HTTP error! Status: ${response.status}`);
+		}
 
-    console.log(response);
+		const data = await response.json();
 
-    if (!response.ok) {
-      throw new Error(`HTTP error! Status: ${response.status}`);
-    }
-    const data = await response.json();
-    var userInfo = new User("dummy mail", "dummy name");
-    userInfo.fullName = data.fullName;
-    userInfo.calories = data.calories;
-    userInfo.proteins = data.proteins;
-    userInfo.carbs = data.carbs;
-    userInfo.fats = data.fats;
-    userInfo.water = data.water;
-    userInfo.targetWeight = data.targetWeight;
+		currentUser.update(data);
 
-    console.debug(data);
+		return data;
+	}
 
-    return data;
-  }
+	public async loginUser(nameArg: string, passwordArg: string) {
+		try {
+			console.log("logging in with url: ", this.url + "Account/Login");
+			const response = await fetch(this.url + "Account/Login", {
+				method: "POST",
+				headers: {
+					Accept: "*/*",
+					"Content-Type": "application/json",
+				},
+				body: JSON.stringify({
+					userName: nameArg,
+					password: passwordArg,
+				}),
+			});
+			if (!response.ok) {
+				throw new Error(`HTTP error! Status: ${response.status}`);
+			}
+			currentUser.token = await response.text();
+			if (currentUser.token === "") {
+				throw new Error("No token received");
+			}
+		} catch (error) {
+			console.error("Error logging in:", error);
+		}
+	}
 
-  public async loginUser(nameArg: string, passwordArg: string) {
-    try {
-      console.log("logging in with url: ", this.url + "Account/Login");
-      const response = await fetch(this.url + "Account/Login", {
-        method: "POST",
-        headers: {
-          Accept: "*/*",
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          userName: nameArg,
-          password: passwordArg,
-        }),
-      });
-      if (!response.ok) {
-        throw new Error(`HTTP error! Status: ${response.status}`);
-      }
-      currentUser.token = await response.text();
-      if (currentUser.token === "") {
-        throw new Error("No token received");
-      }
-    } catch (error) {
-      console.error("Error logging in:", error);
-    }
-  }
-
-  // public async getUser(): Promise<void> {
-  //   await fetch(this.url + `GetUser`)
-  //     .then((response) => {
-  //       if (response.ok) return response.json();
-  //       else throw new Error("getUser fucked up :(");
-  //     })
-  //     .then((data) => {
-  //       console.debug(data);
-  //     });
-  // }
+	// public async getUser(): Promise<void> {
+	//   await fetch(this.url + `GetUser`)
+	//     .then((response) => {
+	//       if (response.ok) return response.json();
+	//       else throw new Error("getUser fucked up :(");
+	//     })
+	//     .then((data) => {
+	//       console.debug(data);
+	//     });
+	// }
 }
 
 export default new Server();
