858d6e174ac859ea07a3a615781e858ad3cee4e3
lukas tast <lukasrtast@yahoo.dk>
2024-05-13 15:13:08 +0200

    added changes to dto

--- UserAppLogic/DTO/AppUserGoalDTO.cs
+++ UserAppLogic/DTO/AppUserGoalDTO.cs
@@ -5,15 +5,15 @@ namespace UserBackend.Data.DTO
     public class AppUserGoalDTO
     {
         [Range(0, double.MaxValue, ErrorMessage = "Target weight must be a positive number.")]
-        public double? TargetWeight { get; set; }
+        public float? TargetWeight { get; set; }
 
         [Range(1, 1.9, ErrorMessage = "Activity level must be between 1 and 1,9.")]
-        public double? activityLevel { get; set; }
+        public float? activityLevel { get; set; }
 
-        [RegularExpression("^(Easy|Medium|Hard)$", ErrorMessage = "Choose a difficulty level between Easy, Medium, or Hard.")]
-        public string? difficultyLevel { get; set; }
+        [Range(200, 800, ErrorMessage = "Choose a difficulty level between Easy, Medium, or Hard.")]
+        public float? difficultyLevel { get; set; }
 
         [Range(0, double.MaxValue, ErrorMessage = "Daily water must be a positive number.")]
-        public double? DailyWater { get; set; }
+        public float? DailyWater { get; set; }
     }
 }
