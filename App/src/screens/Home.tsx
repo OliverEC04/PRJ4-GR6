e41c0979b9081c03e35adfc601da147c9a6cb5ea
lukas tast <lukasrtast@yahoo.dk>
2024-05-14 14:56:18 +0200

    removed errors

--- App/src/screens/Home.tsx
+++ App/src/screens/Home.tsx
@@ -16,7 +16,7 @@ import { User, currentUser } from "../models/User";
 import PopupField from "../components/PopupField";
 import { useFocusEffect } from "@react-navigation/native";
 
-function getCalGoal(user) {
+function getCalGoal(user: User) {
   let bmr;
 
   if (user.gender === "Male") {
@@ -48,24 +48,6 @@ function getFatsGoal(calGoal: number): number {
 }
 
 export default function Home() {
-  const [name, setName] = useState("world");
-  const [calories, setCalories] = useState(0);
-  const [calGoal, setCalGoal] = useState(0);
-  const [calBarColors, setCalBarColors] = useState([
-    "#98C379",
-    "#E5C07B",
-    "#E06C75",
-  ]);
-  const [protein, setProtein] = useState(0);
-  const [proteinGoal, setProteinGoal] = useState(0);
-  const [carbs, setCarbs] = useState(0);
-  const [carbsGoal, setCarbsGoal] = useState(0);
-  const [fats, setFats] = useState(0);
-  const [fatsGoal, setFatsGoal] = useState(0);
-  const [water, setWater] = useState(0);
-  const [waterGoal, setWaterGoal] = useState(0);
-  const [addWaterPopupVisible, setAddWaterPopupVisible] = useState(false);
-  const [showModal, setShowModal] = useState(false);
   const [name, setName] = useState("world");
   const [calories, setCalories] = useState(0);
   const [calGoal, setCalGoal] = useState(0);
@@ -141,91 +123,6 @@ export default function Home() {
     setShowModal(false);
   };
 
-  return (
-    <View style={HomeStyle.container}>
-      <Image
-        source={require("./../../assets/logo.png")}
-        resizeMode="contain"
-        style={HomeStyle.logo}
-      />
-      <Text style={{ fontSize: 30, fontWeight: "200" }}>
-        {`Hello ${name}!`}
-      </Text>
-      <StatBar
-        title="Calories"
-        val={calories}
-        maxVal={calGoal}
-        unit="kcal"
-        height={60}
-        colors={calBarColors}
-      ></StatBar>
-      <StatBar
-        title="Protein"
-        val={protein}
-        maxVal={proteinGoal}
-        unit="g"
-        height={26}
-      ></StatBar>
-      <StatBar
-        title="Carbs"
-        val={carbs}
-        maxVal={carbsGoal}
-        unit="g"
-        height={26}
-      ></StatBar>
-      <StatBar
-        title="Fats"
-        val={fats}
-        maxVal={fatsGoal}
-        unit="g"
-        height={26}
-      ></StatBar>
-      <View style={HomeStyle.waterCont}>
-        <StatBar
-          title="Water"
-          val={water}
-          maxVal={waterGoal}
-          unit="L"
-          height={26}
-          colors={["#E06C75", "#61AFEF"]}
-          width={300 - (60 + 4 + 10)}
-        ></StatBar>
-        <RoundBtn
-          onClick={() => {
-            setAddWaterPopupVisible(true);
-          }}
-          icon={"plus"}
-          size={60}
-          style={HomeStyle.waterBtn}
-        />
-      </View>
-      <PopupField
-        onEnter={(v) => {
-          const water = Number(v);
-          currentUser.currentWater += water;
-          setWater(+currentUser.currentWater.toFixed(3));
-          server.putWater(water);
-        }}
-        visible={addWaterPopupVisible}
-        setVisible={setAddWaterPopupVisible}
-        fieldPlaceholder="Enter liters of water to add"
-        fieldUnit="L"
-      />
-      <Modal visible={showModal} transparent={true}>
-        <View style={HomeStyle.modalView}>
-          <Text style={HomeStyle.modalText}>
-            Welcome to the app! Here are some tips to get you started...
-          </Text>
-          <TouchableOpacity
-            style={HomeStyle.modalBtn}
-            onPress={handleWelcomeButtonPress}
-          >
-            <Text style={HomeStyle.modalBtnText}>Got it!</Text>
-          </TouchableOpacity>
-        </View>
-      </Modal>
-    </View>
-  );
   return (
     <View style={HomeStyle.container}>
       <Image
