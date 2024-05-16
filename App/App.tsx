a84cee37809d4b862358c15c7f4ad00ed7bf958b
au725202 <49028947+Oppai-Loli@users.noreply.github.com>
2024-05-16 16:21:21 +0200

    notification works

--- App/App.tsx
+++ App/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import TabFooter from './src/components/TabFooter';
@@ -12,18 +12,34 @@ AppRegistry.registerComponent('main', () => App);
 const Stack = createNativeStackNavigator();
 
 export default function App() {
-  const [LoggedIn, setLoggedIn] = useState(false);
+  const [loggedIn, setLoggedIn] = useState(false);
+  const notificationCleanupRef = useRef<() => void>(() => {});
 
   useEffect(() => {
-    const LoginStatus = async () => {
+    const loginStatus = async () => {
       const isLogged = await Server.UserLoggedIn();
       setLoggedIn(isLogged);
-      if(isLogged){
-        const cleanup = startNotification();
-        return cleanup;
+      console.log(`User logged in: ${isLogged}`); 
+
+      if (isLogged) {
+        console.log('Starting notification service');
+        notificationCleanupRef.current = startNotification();
+      } else {
+        if (notificationCleanupRef.current) {
+          console.log('Stopping notification service');
+          notificationCleanupRef.current();
+        }
       }
     };
-    LoginStatus();
+
+    loginStatus();
+
+    return () => {
+      if (notificationCleanupRef.current) {
+        console.log('Cleaning up notification service on unmount');
+        notificationCleanupRef.current();
+      }
+    };
   }, []);
 
   return (
